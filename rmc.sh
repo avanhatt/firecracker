dc466e14ffface8135abe98d83ebacaf05a7e113
Alexa VanHattum <alexavanhattum@gmail.com>
2021-09-03 17:45:45 +0000

    Move reachability slice to before solver call

--- rmc.sh
+++ rmc.sh
@@ -6,15 +6,15 @@ ls *.json | parallel -j 72 symtab2gb {} --out {.}.out
 
 mkdir pass_proof_harness
 goto-cc --function pass_proof_harness *.out -o pass_proof_harness/a.out
-goto-instrument --drop-unused-functions pass_proof_harness/a.out pass_proof_harness/b.out
-cbmc pass_proof_harness/b.out --reachability-slice --object-bits 11
+goto-instrument --drop-unused-functions --reachability-slice pass_proof_harness/a.out pass_proof_harness/b.out
+cbmc pass_proof_harness/b.out --object-bits 11
 
 mkdir fail_proof_harness
 goto-cc --function fail_proof_harness *.out -o fail_proof_harness/a.out
-goto-instrument --drop-unused-functions fail_proof_harness/a.out fail_proof_harness/b.out
-cbmc fail_proof_harness/b.out --reachability-slice --object-bits 11
+goto-instrument --drop-unused-functions --reachability-slice fail_proof_harness/a.out fail_proof_harness/b.out
+cbmc fail_proof_harness/b.out --object-bits 11
 
 mkdir balloon_proof_harness
 goto-cc --function balloon_proof_harness *.out -o balloon_proof_harness/a.out
-goto-instrument --drop-unused-functions balloon_proof_harness/a.out balloon_proof_harness/b.out
-cbmc balloon_proof_harness/b.out --reachability-slice --object-bits 11 --unwind 2
+goto-instrument --drop-unused-functions --reachability-slice balloon_proof_harness/a.out balloon_proof_harness/b.out
+cbmc balloon_proof_harness/b.out --object-bits 11 --unwind 2 
