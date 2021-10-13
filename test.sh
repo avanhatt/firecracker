b9c16c68a75ee79599efe1251932db3c842db410
Alexa VanHattum <alexavanhattum@gmail.com>
2021-10-13 22:29:25 +0000

    WIP

--- test.sh
+++ test.sh
@@ -2,13 +2,13 @@
 # RMC PR:  https://github.com/model-checking/rmc/pull/536
 # CBMC PR: https://github.com/diffblue/cbmc/pull/6376
 
-echo "Running just RMC build and CBMC"
+echo "Running RMC build"
 set -e
 rm -rf build
 cd src/devices/src/virtio/
 
 # New flag: -Z restrict_vtable_fn_ptrs
-RUST_BACKTRACE=full RUSTFLAGS="-Z trim-diagnostic-paths=no -Z codegen-backend=gotoc -Z restrict_vtable_fn_ptrs --cfg=rmc" RUSTC=rmc-rustc cargo build  -j 1 --target x86_64-unknown-linux-gnu
+RUST_BACKTRACE=full RUSTFLAGS="-Z trim-diagnostic-paths=no -Z codegen-backend=gotoc -Z restrict_vtable_fn_ptrs --cfg=rmc" RUSTC=rmc-rustc cargo build -j 1 --target x86_64-unknown-linux-gnu
 cd ../../../..
 cd build/cargo_target/x86_64-unknown-linux-gnu/debug/deps/
 ls *.json | parallel -j 72 symtab2gb {} --out {.}.out &> symtab2gb.log
@@ -17,16 +17,35 @@ ls *.json | parallel -j 72 symtab2gb {} --out {.}.out &> symtab2gb.log
 jq -s 'reduce .[] as $item ({}; . * $item)' *.fn_ptr_restrictions > restrictions.json
 
 HARNESS=parse_harness
+rm -rf $HARNESS
 mkdir $HARNESS
-goto-cc --function $HARNESS *.out -o $HARNESS/a.out
 
-# New flag: --function-pointer-restrictions-file all_restrictions.json 
-goto-instrument  --function-pointer-restrictions-file restrictions.json --generate-function-body mmap --generate-function-body-options assert-false --drop-unused-functions --reachability-slice $HARNESS/a.out $HARNESS/b.out 2>&1 | tee $HARNESS/goto-instrument.log
+# C file
+touch empty.c
+goto-cc --function $HARNESS *.out empty.c -o $HARNESS/a.out
+
+# New flag: --function-pointer-restrictions-file all_restrictions.json
+goto-instrument --dump-c $HARNESS/a.out $HARNESS/a.c
+
+goto-instrument --function-pointer-restrictions-file restrictions.json $HARNESS/a.out $HARNESS/b.out 2>&1 | tee $HARNESS/goto-instrument.log
+goto-instrument --dump-c $HARNESS/b.out $HARNESS/b.c
+
 goto-instrument --remove-function-pointers $HARNESS/b.out $HARNESS/c.out 2>&1 | tee $HARNESS/goto-instrument-remove-function-pointers.log
-goto-instrument --dump-c $HARNESS/c.out $HARNESS/genc.c
-time cbmc --trace $HARNESS/b.out --object-bits 11 --unwind 1 2>&1 | tee $HARNESS/cbmc.log
+goto-instrument --dump-c $HARNESS/c.out $HARNESS/c.c
+
+goto-instrument --drop-unused-functions  $HARNESS/c.out $HARNESS/d.out 2>&1 | tee $HARNESS/slice.log
+goto-instrument --dump-c $HARNESS/d.out $HARNESS/d.c
+
+# echo "Running CBMC"
+time cbmc $HARNESS/d.out --object-bits 11 --unwinding-assertions --unwind 2 2>&1 | tee $HARNESS/cbmc.log
+# time cbmc $HARNESS/e.out --object-bits 11 --unwinding-assertions --unwind 2 2>&1 | tee $HARNESS/cbmc.log
+
+# time cbmc $HARNESS/d.out --bounds-check --pointer-check --pointer-primitive-check --conversion-check --div-by-zero-check --float-overflow-check --nan-check --pointer-overflow-check --signed-overflow-check --undefined-shift-check --unsigned-overflow-check --object-bits 11 --unwinding-assertions --unwind 2 2>&1 | tee $HARNESS/cbmc.log
+# [RMC] cmd: cbmc --bounds-check --pointer-check --pointer-primitive-check --conversion-check --div-by-zero-check --float-overflow-check --nan-check --pointer-overflow-check --signed-overflow-check --undefined-shift-check --unsigned-overflow-check --unwinding-assertions --function main src/test/rmc/DynTrait/main.goto
 
 # # echo "Running just CBMC"
-# HARNESS=parse_harness
-# cd /home/ubuntu/firecracker/build/cargo_target/x86_64-unknown-linux-gnu/debug/deps/
-# time cbmc $HARNESS/b.out --object-bits 11 --unwind 2
+# # HARNESS=parse_harness
+# # cd /home/ubuntu/firecracker/build/cargo_target/x86_64-unknown-linux-gnu/debug/deps/
+# # # time cbmc $HARNESS/b.out --object-bits 11 --unwinding-assertions --unwind 2 2>&1 
+# # time cbmc $HARNESS/b.out --object-bits 11 --bounds-check  --unwinding-assertions --unwind 2  2>&1 
+
